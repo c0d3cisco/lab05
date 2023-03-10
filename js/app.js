636db83aea5464e35b34a7369b0a3939c20b7cb3
Francisco Sanchez <c0d3cisco@yahoo.com>
2023-03-10 16:44:26 -0600

    question four

--- js/app.js
+++ js/app.js
@@ -75,7 +75,7 @@ function sumArray(sumArr) { //eslint-disable-line
   let a=testArray[0];
   let b=testArray[1];
   let c=testArray[2];
-  return [sum(sum(a,b)[0],c),`"${a},${b},${c} was passed in as an array of numbers, and ${sum(sum(a,b)[0],c)} is their sum.`];
+  return [sum(sum(a,b)[0],c)[0],`${a},${b},${c} was passed in as an array of numbers, and ${sum(sum(a,b)[0],c)[0]} is their sum.`];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
@@ -96,14 +96,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-  let a=testArray[0];
-  let b=testArray[1];
-  let c=testArray[2];
-  return [multiply(multiply(a,b)[0],c),`"${a},${b},${c} was passed in as an array of numbers, and ${multiply(multiply(a,b)[0],c)} is their sum.`];
+  // let a=testArray[0];
+  // let b=testArray[1];
+  // let c=testArray[2];
+  // return [multiply(multiply(a,b)[0],c),`"${a},${b},${c} was passed in as an array of numbers, and ${multiply(multiply(a,b)[0],c)} is their sum.`];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-testMultiplyArray(testArray);
+//testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
